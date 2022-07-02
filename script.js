a444bc05db0f9b2749eb0c892a87b39de10843ee
Hackzak <92104516+hackzak@users.noreply.github.com>
2022-07-02 08:28:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// //exercice 1
+
+// let nbPneus = `sossisse`;
+
+// let str = `jais besoin de ${nbPneus} pour ma voiture.`;
+
+// console.log(str);
+
+// //exercice 2
+
+// let nb = 10;
+// console.log(nb);
+// // fonction(parametre)
+// function multiplierParDeux(nb) {
+//   return nb * 2;
+// }
+// console.log(multiplierParDeux(55));
+
+// // les condition
+// const mergaise = 1500;
+// const sossisse = 1;
+
+// if (sossisse === 1) {
+//   console.log("double sossisse au chikan negette");
+// } else console.log("double sossisse moisi a la moisissur");
+
+// if (mergaise === 1500) {
+//   console.log("double mergaise au chikan negette");
+// } else console.log("double mergaise moisi a la moisissur");
+
+// //le switch
+
+// let couleur = "vert";
+
+// switch (couleur) {
+//   case "vert":
+//     console.log("vert");
+//     break;
+// }
